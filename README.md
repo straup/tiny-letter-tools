bcaf1d2a9fefae048b63dc2229e5b45ca262cdb8
straup <straup@gmail.com>
2014-06-07 13:09:38 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# tiny-letter-tools
+
+Miscellaneous tools for doing things with Tiny Letter mailing lists.
+
+## mk-rss-feed.py
+
+### For example (abstract)
+
+	url = sys.argv[1]
+	rss = sys.stdout
+
+	tl = TinyLetter(url)
+	tl.as_rss(rss)
+
+### For example (concrete)
+
+	$> python ./mk-rss-feed.py https://tinyletter.com/danhon/ > danhon.rss
+	$> xmllint --format danhon.rss
+
+	<?xml version="1.0" encoding="iso-8859-1"?>
+	<rss version="2.0">
+	  <channel>
+	    <title> Things That Have Caught My Attention by danhon </title>
+	    <link>https://tinyletter.com/danhon/</link>
+	    <description> by danhon </description>
+	    <lastBuildDate>Sat, 07 Jun 2014 12:50:04 GMT</lastBuildDate>
+	    <generator>PyRSS2Gen-1.0.0</generator>
+	    <docs>http://blogs.law.harvard.edu/tech/rss</docs>
+	    <item>
+	      <title>Episode Ninety Seven: That Most Killer Of Deals; Need To Know</title>
+	      <link>https://tinyletter.com/danhon/letters/</link>
+	      <description>I got a bunch of notes back about what I wrote the other day about my depression. I don't really mind writing about it, and I think it's a tragedy that more people don't understand how someone can loo</description>
+	      <guid>a3f6a69df8a62e61951f25f3dc1d06f6</guid>
+	      <pubDate>Sat, 07 Jun 2014 00:00:00 GMT</pubDate>
+	    </item>
+	    <item>
+	      <title>Episode Ninety Six: And Then It Came Back; Meek, Meeker, Meekest</title>
+	      <link>https://tinyletter.com/danhon/letters/episode-ninety-six-and-then-it-came-back-meek-meeker-meekest</link>
+	      <description>A difficult day, as you'll see below. And the baby monitor app isn't working, so I'm upstairs, typing quietly and quickly whilst watching my son sleep. But, tomorrow's Friday, and my one meeting is ab</description>
+	      <guid>b22f44bd1ff7fcb4bb0a1aa5ab98c61a</guid>
+	      <pubDate>Fri, 06 Jun 2014 00:00:00 GMT</pubDate>
+	    </item>
+	
+	<-- and so on.. -->
+
+### Dependencies
+
+* https://pypi.python.org/pypi/BeautifulSoup/
+* PyRSS2Gen
+
+## See also
+
+* http://tinyletter.com
---
+++ bin/mk-rss-feed.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+import sys
+import urllib
+import urlparse
+import logging
+import datetime
+import hashlib
+ 
+import PyRSS2Gen
+from BeautifulSoup import BeautifulSoup
+
+class UserAgent(urllib.FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15'
+ 
+class TinyLetter:
+    
+    def __init__(self, url):
+
+        if not url.endswith('/'):
+            url = url + '/'
+
+        self.url = url
+        self.ua = UserAgent()
+
+    def as_rss(self, fh):
+
+        page = self.ua.open(self.url) 
+        text = page.read()
+        page.close()
+ 
+        soup = BeautifulSoup(text)
+        title = soup.title.string
+
+        byline = soup.findAll('span', {'class': 'byline'})
+        byline = byline[0]
+        byline = byline.string
+
+        items = []
+
+        letters = self.url + 'letters/'
+
+        for item in self.process_as_rss(letters):
+            items.append(PyRSS2Gen.RSSItem(**item))
+
+        now = datetime.datetime.now()
+
+        rss = PyRSS2Gen.RSS2(
+            title = title,
+            link = url,
+            description = byline,
+            lastBuildDate = now,
+            items = items
+        )
+
+        rss.write_xml(fh)
+
+    def process_as_rss(self, url, items=[], max_items=15):
+
+        logging.debug("parsing %s" % url)
+
+        try:
+            page = self.ua.open(url)
+            text = page.read()
+            page.close()
+
+        except Exception, e:
+            logging.error("Failed to open %s, because %s" % (url, e))
+            return items
+ 
+        try:
+            soup = BeautifulSoup(text)
+        except Exception, e:
+            logging.error("Failed to parse text, because %s" % e)
+            return items
+
+        link = url
+
+        # Insert sound of old timers chuckling here...
+
+        try:
+            title = soup.find('meta', {'name': 'twitter:title'})
+            title = title['content']
+        except TypeError, e:
+            title = soup.find('meta', {'property': 'og:title'})
+            title = title['content']
+        except Exception, e:
+            logging.error("Failed to parse title, because %s" % e)
+            title = url
+
+        try:
+            desc = soup.find('meta', {'name': 'twitter:description'})
+            desc = desc['content']
+        except TypeError, e:
+            desc = soup.find('meta', {'property': 'og:description'})
+            desc = desc['content']
+        except Exception, e:
+            logging.error("Failed to parse description, because %s" % e)
+            desc = ""
+
+        try:
+            header = soup.find('div', {'id': 'message-heading'})
+            date = header.find('div', {'class': 'date'})
+            date = date.string
+            date = date.strip()
+            pubdate = datetime.datetime.strptime(date, "%B %d, %Y") 
+
+        except Exception, e:
+            logging.error("Failed to parse date because %s, so just using 'now' instead" % e)
+            pubdate = datetime.datetime.now()
+            
+        hash = hashlib.md5()
+        hash.update(link)
+        guid = hash.hexdigest()
+
+        item = {
+            'title': title,
+            'link': link,
+            'description': desc,
+            'guid': guid,
+            'pubDate': pubdate
+        }
+
+        items.append(item)
+
+        if len(items) == max_items:
+            logging.debug("Reached max items limit, all done")
+            return items
+
+        for tag in soup.findAll('a', href=True):
+            
+            css = tag.get('class', None)
+            
+            if not css:
+                continue
+                
+            if not css == 'paging-button next':
+                continue
+
+            next = 'https://tinyletter.com' + tag['href']
+            items = self.process_as_rss(next, items)
+
+        return items
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    rss = sys.stdout
+
+    tl = TinyLetter(url)
+    tl.as_rss(rss)
